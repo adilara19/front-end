bdd95b1f1cf3bd045fe2cdc5cce872a0a9938874
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2023-08-10 15:55:20 +0300

    about sayfası eklendi

---
+++ resources/views/about.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('title') ABOUT US @endsection
+@section('css')
+<style>
+body{
+    background: #fff;
+}
+
+</style>
+@endsection
+
+@section('content')
+<div class="row">
+      <h2>About Us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea distinctio officiis expedita, ratione officia rerum incidunt hic cupiditate tenetur perferendis eaque itaque quod nobis quaerat magni mollitia illo numquam? Consectetur?
+      Aspernatur, dolorem. Fugiat quisquam ipsam ullam perferendis, magnam ab nisi omnis laudantium nihil minima ex quas, dignissimos natus assumenda! Ea expedita iste natus libero eos voluptatum neque iusto sed sit.
+      Magnam quibusdam temporibus ea libero, eos assumenda cum? Rem cumque, iusto accusantium voluptate laudantium dolorem vel fugit quis nihil corporis quos repellat alias commodi at deserunt eius quaerat, culpa beatae.
+      Id tenetur iusto repellendus quos voluptatem molestias a atque illum maiores eligendi maxime at reiciendis temporibus aliquid iste necessitatibus vero dolore doloremque eum omnis esse quis, accusantium sit magni. Sunt.
+      Saepe nobis recusandae sapiente adipisci? Reprehenderit placeat eum assumenda dolore dolor aspernatur distinctio adipisci unde aliquam quibusdam at pariatur nesciunt, exercitationem enim, dignissimos fuga mollitia culpa accusamus eligendi aliquid cupiditate!</p>
+</div>
+@endsection
+
+@section('js')
+<script src="#"></script>
+@endsection
